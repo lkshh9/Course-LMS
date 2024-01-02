81a712ed3d2d543094b8bdc1efb190423704f8fc
Lokesh <lokeshmarwal007@gmail.com>
2024-01-02 20:41:20 +0530

    created description-form and image-form component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,8 @@ import { auth } from "@clerk/nextjs"
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
 
 const CourseIdPage = async ({
     params
@@ -56,6 +58,14 @@ const CourseIdPage = async ({
                         initialData={course}
                         courseId={course.id}
                      />
+                    <DescriptionForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <ImageForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
